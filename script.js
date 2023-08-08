d465e3f9092ebb22163fb0524590f40116eb89fc
mohdimtiyaz002 <76655817+mohdimtiyaz002@users.noreply.github.com>
2023-08-08 20:40:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// Get the reference to the button and paragraph elements
 const colorButton = document.getElementById('colorButton');
 const textParagraph = document.getElementById('textParagraph');
 
+// Add a click event listener to the button
 colorButton.addEventListener('click', function() {
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    // Generate a random color
+    const randomColor = getRandomColor();
+
+    // Change the text color of the paragraph
     textParagraph.style.color = randomColor;
 });
+
+// Function to generate a random color in hexadecimal format
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
